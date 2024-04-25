1ed94a98cca12e59d89f70044f12b010a2d2cddb
AnkitSinghSengarCygbit <ankit.sengar@cygbit.com>
2024-04-25 14:59:58 +0530

    commit by Ankit

---
+++ App.tsx
@@ -0,0 +1,83 @@
+import {FlatList, StyleSheet, View, ViewToken} from 'react-native';
+import React from 'react';
+import Animated, {
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from 'react-native-reanimated';
+import data, {OnboardingData} from './src/data/data';
+import RenderItem from './src/components/RenderItem';
+import Pagination from './src/components/Pagination';
+import CustomButton from './src/components/CustomButton';
+
+const App = () => {
+  const flatlistRef = useAnimatedRef<FlatList<OnboardingData>>();
+  const x = useSharedValue(0);
+  const flaslistIndex = useSharedValue(0);
+
+  const onViewableItemsChanged = ({
+    viewableItems,
+  }: {
+    viewableItems: ViewToken[];
+  }) => {
+    if (viewableItems[0].index !== null) {
+      flaslistIndex.value = viewableItems[0].index;
+    }
+  };
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: event => {
+      x.value = event.contentOffset.x;
+    },
+  });
+  return (
+    <View style={styles.container}>
+      <Animated.FlatList
+        ref={flatlistRef}
+        onScroll={onScroll}
+        data={data}
+        renderItem={({item, index}) => {
+          return <RenderItem item={item} index={index} x={x} />;
+        }}
+        keyExtractor={item => item.id.toString()}
+        scrollEventThrottle={16}
+        horizontal={true}
+        bounces={false}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={{
+          minimumViewTime: 300,
+          viewAreaCoveragePercentThreshold: 10,
+        }}
+      />
+      <View style={styles.bottomContainer}>
+        <Pagination data={data} x={x} />
+        <CustomButton
+          flatListRef={flatlistRef}
+          flatListIndex={flaslistIndex}
+          dataLength={data.length}
+          x={x}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+    marginHorizontal: 30,
+    paddingVertical: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
